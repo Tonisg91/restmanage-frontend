25726e35b75356bb1931cc05fe1f5b97b827d664
Tonisg91 <sancheztoni.garcia@gmail.com>
2020-09-16 09:24:30 +0200

    Limpieza general de codigo

--- src/components/admin-side/dailyMenu/DailyMenuForm.jsx
+++ src/components/admin-side/dailyMenu/DailyMenuForm.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
 import dailyMenuService from '../../../tools/dailyMenuService'
 import { StyledFormDailyMenu } from '../../styled-components/admin-side'
 
 function DailyMenuForm() {
-    const history = useHistory()
     const dispatch = useDispatch()
     const dailyMenu = useSelector(state => state.dailyMenu)
     const initialForm = {
